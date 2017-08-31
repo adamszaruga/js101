68346e1444035bdf4b8d3c33d7a05417aed0b3e6
Adam Szaruga <adam@users-MacBook-Air.local>
2017-08-31 17:07:57 -0400

    initial commit

---
+++ exercises/level5.js
@@ -0,0 +1,25 @@
+function lvl5exercise1() {
+	// Push the string "hello" into the array and return it.
+	var array = [1, "adam"];
+	
+}
+
+function lvl5exercise2() {
+	// Remove the last element from the array and return it
+	var array = [1, "adam"];
+
+}
+
+function lvl5exercise3() {
+	// Return the length of the array
+	var array = [1, "adam"];
+
+}
+
+function lvl5exercise4() {
+	// Return the index of "adam" in the array
+	var array = [1, "adam"];
+
+}
+
+
---
+++ exercises/level6.js
@@ -0,0 +1,19 @@
+function lvl6exercise1(num) {
+	// Return 'hello' if num is 1, 'world' if num is 2, otherwise return'bye'
+	
+}
+
+function lvl6exercise2() {
+	// Push 10 "hello" strings into the array using a for loop, then return it
+	var array = [];
+
+}
+
+function lvl6exercise3() {
+	// Empty this array using a while loop and return it
+	var array = ["hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello"];
+
+}
+
+
+
